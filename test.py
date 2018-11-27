bed24972184ecd8e57276922ceed3aac9f89b0ed
Brandon S <atomicbcs@gmail.com>
2018-11-26 16:21:53 -0800

    got something workng

---
+++ test.py
@@ -0,0 +1,101 @@
+from IPython import display
+import math
+import configparser
+from pprint import pprint
+import pandas as pd
+import numpy as np
+import nltk
+import matplotlib.pyplot as plt
+import seaborn as sns
+from nltk.tokenize import word_tokenize, RegexpTokenizer
+sns.set(style='darkgrid', context='talk', palette='Dark2')
+
+import praw
+
+try:
+    f = open('keys.cfg');
+    config = configparser.ConfigParser()
+    config.readfp(f)
+    client_secret = config._sections["reddit"]['api_secret']
+    client_id = config._sections["reddit"]['client_id']
+    user_agent = config._sections["reddit"]['username']
+except:
+    raise Exception("Invalid config")
+
+reddit = praw.Reddit(client_id=client_id,
+                     client_secret=client_secret,
+                     user_agent=user_agent)
+
+headlines = set()
+
+for submission in reddit.subreddit('gaming').new(limit=None):
+    headlines.add(submission.title)
+    display.clear_output()
+    print(len(headlines))
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+
+sia = SIA()
+results = []
+
+for line in headlines:
+    pol_score = sia.polarity_scores(line)
+    pol_score['headline'] = line
+    results.append(pol_score)
+
+pprint(results[:3], width=100)
+
+df = pd.DataFrame.from_records(results)
+df.head()
+
+df['label'] = 0
+df.loc[df['compound'] > 0, 'label'] = 1
+df.loc[df['compound'] < 0, 'label'] = -1
+df.head()
+
+df2 = df[['headline', 'label']]
+df2.to_csv('reddit_headlines_labels.csv', mode='a', encoding='utf-8', index=False)
+
+print("Positive headlines:\n")
+pprint(list(df[df['label'] == 1].headline)[:5], width=200)
+
+print("\nNegative headlines:\n")
+pprint(list(df[df['label'] == -1].headline)[:5], width=200)
+
+print(df.label.value_counts())
+
+print(df.label.value_counts(normalize=True) * 100)
+
+fig, ax = plt.subplots(figsize=(8, 8))
+
+counts = df.label.value_counts(normalize=True) * 100
+
+sns.barplot(x=counts.index, y=counts, ax=ax)
+
+ax.set_xticklabels(['Negative', 'Neutral', 'Positive'])
+ax.set_ylabel("Percentage")
+
+plt.show()
+
+from nltk.corpus import stopwords
+
+stop_words = stopwords.words('english')
+
+tokenizer = RegexpTokenizer(r'\w+')
+
+def process_text(headlines):
+    tokens = []
+    for line in headlines:
+        toks = tokenizer.tokenize(line)
+        toks = [t.lower() for t in toks if t.lower() not in stop_words]
+        tokens.extend(toks)
+    
+    return tokens
+
+
+pos_lines = list(df[df.label == 1].headline)
+
+pos_tokens = process_text(pos_lines)
+pos_freq = nltk.FreqDist(pos_tokens)
+
+print(pos_freq.most_common(20))
